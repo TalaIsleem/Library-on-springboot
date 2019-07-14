c69419de7da11146402cda06993777a37643b8c1
DaliaB <dalia.batyeh@exalt.ps>
2019-07-14 16:36:01 +0300

    added

--- main/java/io/boksLibrary/author/AuthorRepository.java
+++
@@ -1,8 +0,0 @@
-package io.boksLibrary.author;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthorRepository extends JpaRepository <Author, Long>  {
-
-}
--- main/java/io/boksLibrary/user/employee/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package io.boksLibrary.user.employee;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface EmployeeRepository extends JpaRepository <Employee, Long> {
-		
-	
-}
